4e27d125d7e7ee36cb3fc217c5e628efd90ace75
Marc Van Olmen <marc.vanolmen@thomsonreuters.com>
2018-04-09 18:30:34 -0400

    [add] added carthage support.

---
+++ appboy-segment-ios/appboy_segment_ios.h
@@ -0,0 +1,19 @@
+//
+//  appboy_segment_ios.h
+//  appboy-segment-ios
+//
+//  Created by Marc Van Olmen on 4/9/18.
+//  Copyright © 2018 Marc Van Olmen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for appboy_segment_ios.
+FOUNDATION_EXPORT double appboy_segment_iosVersionNumber;
+
+//! Project version string for appboy_segment_ios.
+FOUNDATION_EXPORT const unsigned char appboy_segment_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <appboy_segment_ios/PublicHeader.h>
+
+
